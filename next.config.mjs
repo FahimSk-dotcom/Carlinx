1c9ca827530938466aee717fe9b6d4b39746c044
FahimShaikh <skfahim0504@gmail.com>
2025-01-16 20:00:40 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Enables static export
+  basePath: '/Carlinx', // Replace with your GitHub repository name
 };
 
 export default nextConfig;
